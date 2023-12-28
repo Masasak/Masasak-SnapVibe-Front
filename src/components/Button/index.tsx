0c3e4ec5bc213acd9a2888afb80d90f6cca192f6
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-12-28 19:55:58 +0900

    test

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import * as S from "./style";
+import * as S from './style';
 
 const Button = () => {
-  return <S.ButtonWrap>버튼입니다.</S.ButtonWrap>;
+	return <S.ButtonWrap>테스트</S.ButtonWrap>;
 };
 
 export default Button;
