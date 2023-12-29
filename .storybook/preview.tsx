877ec93ac0e9b1b988b2dcc7aa17ea0f219301eb
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-12-29 23:49:11 +0900

    Update format prettier

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,39 +1,39 @@
-"use client";
+'use client';
 
-import type { Preview } from "@storybook/react";
-import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 
-import { GlobalStyle } from "../src/styles";
-import React from "react";
+import { GlobalStyle } from '../src/styles';
+import React from 'react';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-      backgrounds: {
-        default: "light",
-        values: [
-          {
-            name: "light",
-            value: "#ffffff",
-          },
-          {
-            name: "dark",
-            value: "#050505",
-          },
-        ],
-      },
-      decorators: [
-        withThemeFromJSXProvider({
-          GlobalStyles: GlobalStyle,
-        }),
-      ],
-    },
-  },
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+			backgrounds: {
+				default: 'light',
+				values: [
+					{
+						name: 'light',
+						value: '#ffffff',
+					},
+					{
+						name: 'dark',
+						value: '#050505',
+					},
+				],
+			},
+			decorators: [
+				withThemeFromJSXProvider({
+					GlobalStyles: GlobalStyle,
+				}),
+			],
+		},
+	},
 };
 
 export default preview;
