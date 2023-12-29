c6b27a7664af2ddde79119afcf8e61d10c979415
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-12-29 23:56:39 +0900

    Update tap width

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -7,33 +7,33 @@ import { GlobalStyle } from '../src/styles';
 import React from 'react';
 
 const preview: Preview = {
-	parameters: {
-		actions: { argTypesRegex: '^on[A-Z].*' },
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-			backgrounds: {
-				default: 'light',
-				values: [
-					{
-						name: 'light',
-						value: '#ffffff',
-					},
-					{
-						name: 'dark',
-						value: '#050505',
-					},
-				],
-			},
-			decorators: [
-				withThemeFromJSXProvider({
-					GlobalStyles: GlobalStyle,
-				}),
-			],
-		},
-	},
+ parameters: {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+   matchers: {
+    color: /(background|color)$/i,
+    date: /Date$/i,
+   },
+   backgrounds: {
+    default: 'light',
+    values: [
+     {
+      name: 'light',
+      value: '#ffffff',
+     },
+     {
+      name: 'dark',
+      value: '#050505',
+     },
+    ],
+   },
+   decorators: [
+    withThemeFromJSXProvider({
+     GlobalStyles: GlobalStyle,
+    }),
+   ],
+  },
+ },
 };
 
 export default preview;
