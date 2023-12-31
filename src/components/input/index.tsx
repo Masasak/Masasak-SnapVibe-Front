2ad21058e62bb6357b2298cd15121677497eacc4
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-12-31 22:59:18 +0900

    Update prettier

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -5,34 +5,34 @@ import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { useRef } from 'react';
 
 interface Props {
- inputTitle: string;
- inputWidth?: 'long';
+  inputTitle: string;
+  inputWidth?: 'long';
 }
 
 const Input = ({ inputTitle, inputWidth }: Props) => {
- const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
- const width = '19.875rem';
- const longWidth = '27.1875rem';
+  const width = '19.875rem';
+  const longWidth = '27.1875rem';
 
- const handleDivClick = () => {
-  if (inputRef.current) {
-   inputRef.current.focus();
-  }
- };
+  const handleDivClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
- return (
-  <div
-   className={S.InputWrap}
-   style={assignInlineVars({
-    [S.widthVar]: inputWidth === 'long' ? longWidth : width,
-   })}
-   onClick={handleDivClick}
-  >
-   <p className={S.InputTitle}>{inputTitle}</p>
-   <input type="text" className={S.InputContent} ref={inputRef} />
-  </div>
- );
+  return (
+    <div
+      className={S.InputWrap}
+      style={assignInlineVars({
+        [S.widthVar]: inputWidth === 'long' ? longWidth : width,
+      })}
+      onClick={handleDivClick}
+    >
+      <p className={S.InputTitle}>{inputTitle}</p>
+      <input type="text" className={S.InputContent} ref={inputRef} />
+    </div>
+  );
 };
 
 export default Input;
