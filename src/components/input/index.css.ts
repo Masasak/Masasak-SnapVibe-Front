2fa2738dacabcc80a5a9004caa4595951d9db151
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-12-31 22:56:01 +0900

    Update prettier

--- src/components/input/index.css.ts
+++ src/components/input/index.css.ts
@@ -3,33 +3,33 @@ import { style, createVar } from '@vanilla-extract/css';
 export const widthVar = createVar();
 
 export const InputWrap = style({
-	width: widthVar,
-	height: '4.375rem',
-	backgroundColor: '#2F2E4A',
-	borderRadius: '0.625rem',
-	padding: '0.5rem 0rem 1.5625rem 1rem',
-	boxSizing: 'border-box',
-	cursor: 'text',
+  width: widthVar,
+  height: '4.375rem',
+  backgroundColor: '#2F2E4A',
+  borderRadius: '0.625rem',
+  padding: '0.5rem 0rem 1.5625rem 1rem',
+  boxSizing: 'border-box',
+  cursor: 'text',
 });
 
 export const InputTitle = style({
-	fontSize: '0.875rem',
-	fontStyle: 'normal',
-	fontWeight: '400',
-	color: '#777777',
-	paddingBottom: '0.1875rem',
-	margin: '0',
+  fontSize: '0.875rem',
+  fontStyle: 'normal',
+  fontWeight: '400',
+  color: '#777777',
+  paddingBottom: '0.1875rem',
+  margin: '0',
 });
 
 export const InputContent = style({
-	width: '90%',
-	height: '1.1875rem',
-	fontSize: '1rem',
-	fontStyle: 'normal',
-	fontWeight: '400',
-	background: 'none',
-	color: '#ffffff',
-	border: 'none',
-	outline: 'none',
-	padding: '0',
+  width: '90%',
+  height: '1.1875rem',
+  fontSize: '1rem',
+  fontStyle: 'normal',
+  fontWeight: '400',
+  background: 'none',
+  color: '#ffffff',
+  border: 'none',
+  outline: 'none',
+  padding: '0',
 });
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -5,51 +5,51 @@ import { Global, css } from '@emotion/react';
 import emotionReset from 'emotion-reset';
 
 export function GlobalStyle() {
-	return (
-		<Global
-			styles={css`
-				${emotionReset}
+  return (
+    <Global
+      styles={css`
+        ${emotionReset}
 
-				body {
-					overscroll-behavior-y: none;
-					overflow-x: hidden;
-				}
+        body {
+          overscroll-behavior-y: none;
+          overflow-x: hidden;
+        }
 
-				@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css');
-				@import url('https://use.typekit.net/cei0oyt.css');
+        @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css');
+        @import url('https://use.typekit.net/cei0oyt.css');
 
-				body,
-				* {
-					font-family:
-						'Pretendard Variable',
-						Pretendard,
-						-apple-system,
-						BlinkMacSystemFont,
-						system-ui,
-						roboto,
-						'Helvetica Neue',
-						'Segoe UI',
-						'Apple SD Gothic Neo',
-						'Noto Sans KR',
-						'Malgun Gothic',
-						'Apple Color Emoji',
-						'Segoe UI Emoji',
-						'Segoe UI Symbol',
-						sans-serif;
-					box-sizing: border-box;
-					padding: 0;
-					margin: 0;
-				}
+        body,
+        * {
+          font-family:
+            'Pretendard Variable',
+            Pretendard,
+            -apple-system,
+            BlinkMacSystemFont,
+            system-ui,
+            roboto,
+            'Helvetica Neue',
+            'Segoe UI',
+            'Apple SD Gothic Neo',
+            'Noto Sans KR',
+            'Malgun Gothic',
+            'Apple Color Emoji',
+            'Segoe UI Emoji',
+            'Segoe UI Symbol',
+            sans-serif;
+          box-sizing: border-box;
+          padding: 0;
+          margin: 0;
+        }
 
-				a {
-					text-decoration: none;
-					color: inherit;
-				}
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
 
-				input {
-					font-family: 'Pretendard';
-				}
-			`}
-		/>
-	);
+        input {
+          font-family: 'Pretendard';
+        }
+      `}
+    />
+  );
 }
