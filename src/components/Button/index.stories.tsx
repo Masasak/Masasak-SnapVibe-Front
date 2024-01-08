4898fe60582a1ca7be911c07f4b340f4af7514bf
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-01-08 15:58:00 +0900

    Add Button component story

---
+++ src/components/Button/index.stories.tsx
@@ -0,0 +1,21 @@
+import Button from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta<typeof Button>;
+
+type Story = StoryObj<typeof Button>;
+
+export const PrimaryButton: Story = {
+  args: {
+    children: '로그인',
+  },
+};
+export const SubButton: Story = {
+  args: {
+    children: '신고하기',
+  },
+};
