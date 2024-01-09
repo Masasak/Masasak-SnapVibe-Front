7f8e8df58227e593077234c6a0523222d4c0a625
TaerangLee <s22051@gsm.hs.kr>
2024-01-09 16:16:46 +0900

    Add export svg

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as LogoIcon } from './LogoIcon';
+export { default as ChatIcon } from './ChatIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as UserIcon } from './UserIcon';
+export { default as NoticeIcon } from './NoticeIcon';
