ba5d2c9ac57fe96375a3071e5d11fb82091b8bc8
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-01-09 21:00:17 +0900

    Add InputForm export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from './Input';
 export { default as PrimaryButton } from './PrimaryButton';
 export { default as SubButton } from './SubButton';
+export { default as InputFrom } from './InputForm';
