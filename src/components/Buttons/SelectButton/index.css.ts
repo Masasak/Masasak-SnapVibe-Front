9239a741c4317987f654165bfea98a3722a4a654
TaerangLee <s22051@gsm.hs.kr>
2024-01-10 21:13:57 +0900

    Update select button

---
+++ src/components/Buttons/SelectButton/index.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css';
+import { theme } from 'styles';
+
+export const ButtonStyle = style({
+  width: '27.1875rem',
+  height: '4.375rem',
+  borderRadius: '0.625rem',
+  backgroundColor: theme.color.purple2,
+  color: theme.grayScale.white,
+  ...theme.typo.Body2,
+
+  ':hover': {
+    border: '1px solid white',
+  },
+});
---
+++ src/components/Buttons/SelectButton/index.stories.tsx
@@ -0,0 +1,22 @@
+import SelectButton from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'SelectButton',
+  component: SelectButton,
+} as Meta<typeof SelectButton>;
+
+type Story = StoryObj<typeof SelectButton>;
+
+export const Primary: Story = {
+  args: {
+    children: '잔임함 ( 부적절한 이미지나 혐오적인 이미지을 올린 경우 )',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: '성적임 ( 부적절한 발언이나 성적인 이미지를 유포한 경우 )',
+  },
+};
