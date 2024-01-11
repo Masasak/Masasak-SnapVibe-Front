433d9ac4a4f26d6db9a1b9ae7e5c72699c388855
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-01-11 23:21:51 +0900

    Add HeartIcon

---
+++ src/assets/svg/HeartIcon.tsx
@@ -0,0 +1,18 @@
+const HeartIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.375rem"
+    height="1.25rem"
+    viewBox="0 0 22 20"
+    fill="none"
+  >
+    <path
+      d="M21 5.90909C21 3.19818 18.6678 1 15.7911 1C13.6411 1 11.7944 2.22836 11 3.98145C10.2056 2.22836 8.35889 1 6.20778 1C3.33333 1 1 3.19818 1 5.90909C1 13.7855 11 19 11 19C11 19 21 13.7855 21 5.90909Z"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default HeartIcon;
