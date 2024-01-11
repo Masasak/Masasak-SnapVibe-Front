38dd6d40859cc7a0c6ca73457068e5fa010f7192
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-01-11 23:21:59 +0900

    Update Icon export

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,3 +2,7 @@ export { default as ChatIcon } from './ChatIcon';
 export { default as SearchIcon } from './SearchIcon';
 export { default as UserIcon } from './UserIcon';
 export { default as NoticeIcon } from './NoticeIcon';
+export { default as FlagIcon } from './FlagIcon';
+export { default as HeartIcon } from './HeartIcon';
+export { default as ShareIcon } from './ShareIcon';
+export { default as ReportIcon } from './ReportIcon';
