7acb0fd9470e1c3e5d323b2f79e7e05962443c90
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-01-16 15:41:26 +0900

    Update heart Icon conditional styling

--- src/assets/svg/HeartIcon.tsx
+++ src/assets/svg/HeartIcon.tsx
@@ -1,4 +1,8 @@
-const HeartIcon = () => (
+interface HeartIconProps {
+  isLiked?: boolean;
+}
+
+const HeartIcon: React.FC<HeartIconProps> = ({ isLiked }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1.375rem"
@@ -8,9 +12,10 @@ const HeartIcon = () => (
   >
     <path
       d="M21 5.90909C21 3.19818 18.6678 1 15.7911 1C13.6411 1 11.7944 2.22836 11 3.98145C10.2056 2.22836 8.35889 1 6.20778 1C3.33333 1 1 3.19818 1 5.90909C1 13.7855 11 19 11 19C11 19 21 13.7855 21 5.90909Z"
-      stroke="white"
       strokeLinecap="round"
       strokeLinejoin="round"
+      fill={isLiked ? 'red' : 'none'}
+      stroke={isLiked ? 'red' : 'white'}
     />
   </svg>
 );
--- src/components/PostCard/index.css.ts
+++ src/components/PostCard/index.css.ts
@@ -1,9 +1,10 @@
 import { style } from '@vanilla-extract/css';
+import { theme } from 'styles';
 
 export const PostCardWrap = style({
   width: '26.25rem',
   height: '26.25rem',
-  backgroundColor: 'red',
+  backgroundColor: '#222222',
   borderRadius: '0.625rem',
   position: 'relative',
 });
@@ -11,7 +12,6 @@ export const PostCardWrap = style({
 export const PostCardImgWrapper = style({
   width: '26.25rem',
   height: '17.5rem',
-  backgroundColor: 'yellow',
   borderTopLeftRadius: '0.625rem',
   borderTopRightRadius: '0.625rem',
   position: 'relative',
@@ -28,6 +28,10 @@ export const HeartIconWrapper = style({
   zIndex: '1',
   left: '5%',
   top: '3.8%',
+});
+
+export const IconWrapper = style({
+  cursor: 'pointer',
 });
 
 export const ProfileImgWrapper = style({
@@ -50,5 +54,63 @@ export const CardTitleWrapper = style({
   width: '100%',
   height: '1.5rem',
   marginTop: '2.0625rem',
-  backgroundColor: 'yellow',
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+export const ContentWrapper = style({
+  display: 'flex',
+});
+
+export const CardIconWrapper = style({
+  width: '4rem',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+export const UserId = style({
+  ...theme.typo.Body2,
+  fontWeight: '500',
+  color: theme.grayScale.white,
+  marginRight: '0.75rem',
+  cursor: 'pointer',
+});
+
+export const FollowTitle = style({
+  ...theme.typo.Body3,
+  fontWeight: '500',
+  color: theme.color.blue2,
+  paddingTop: '1px',
+  cursor: 'pointer',
+});
+
+export const CardSubTitleWrapper = style({
+  marginTop: '0.25rem',
+  width: '86.6%',
+  display: 'flex',
+  gap: '0.5rem',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+});
+
+export const HashTagStyle = style({
+  ...theme.typo.Body3,
+  color: theme.color.blue2,
+  fontWeight: '500',
+});
+
+export const SubTitleStyle = style({
+  ...theme.typo.Body3,
+  color: theme.grayScale.white,
+  fontWeight: '500',
+});
+
+export const WriteCommentTitle = style({
+  ...theme.typo.Body3,
+  color: theme.grayScale.gray2,
+  fontWeight: '500',
+  marginTop: '0.9375rem',
+  cursor: 'pointer',
 });
