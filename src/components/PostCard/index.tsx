928312d6ecaa3b57761e56ceafc5545a551abbc2
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-01-16 15:39:51 +0900

    Update post card publishing

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -16,7 +16,7 @@ const PostCard = () => {
           fill
           className={S.Img}
           src="https://i.namu.wiki/i/kZ7tWSGl2Y5JbYfQyyE4b0UU6YYtlQ8ep-snNnPY6pPVXPDQ4nvs2GEr0LQj6WrNlzVEBxA1WHr7pcQV4nMuIQ.webp"
-          alt=""
+          alt="게시물사진"
         />
       </div>
       <div className={S.ProfileImgWrapper}>
@@ -24,11 +24,31 @@ const PostCard = () => {
           fill
           className={S.Img}
           src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FRGeI6%2Fbtq21db32du%2F84vjG2iZc3H6iuYnUKkb7k%2Fimg.jpg"
-          alt=""
+          alt="프러ㅍ"
         />
       </div>
       <div className={S.CardContentWrapper}>
-        <div className={S.CardTitleWrapper}></div>
+        <div className={S.CardTitleWrapper}>
+          <div className={S.ContentWrapper}>
+            <p className={S.UserId}>dongwook1207</p>
+            <p className={S.FollowTitle}>팔로우</p>
+          </div>
+          <div className={S.CardIconWrapper}>
+            <div className={S.IconWrapper}>
+              <I.ShareIcon />
+            </div>
+            <div className={S.IconWrapper}>
+              <I.FlagIcon />
+            </div>
+          </div>
+        </div>
+        <div className={S.CardSubTitleWrapper}>
+          <p className={S.HashTagStyle}># newjeans</p>
+          <p className={S.SubTitleStyle}>Asap Ditto hipe Boy</p>
+          <p className={S.HashTagStyle}># haerin</p>
+          <p className={S.SubTitleStyle}>뉴진스 이정우</p>
+        </div>
+        <p className={S.WriteCommentTitle}>댓글 달기...</p>
       </div>
     </div>
   );
