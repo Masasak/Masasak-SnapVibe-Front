5a181518b12117ee99604253ff7070e3046feb5a
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-02-03 00:17:03 +0900

    Add CancelIcon

---
+++ src/assets/svg/CancelIcon.tsx
@@ -0,0 +1,19 @@
+const CacnelIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.5rem"
+    height="1.5rem"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M6 18L18 6M6 6L18 18"
+      stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default CacnelIcon;
