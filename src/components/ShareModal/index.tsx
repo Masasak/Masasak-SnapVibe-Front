5a4fe9f464bc1f589dbbde2cd7e04494117890c2
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-02-03 03:05:05 +0900

    Update maintain user selected

--- src/components/ShareModal/index.tsx
+++ src/components/ShareModal/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import { shareUserTestData } from '../../../public/shareUserTest';
 
@@ -12,34 +12,60 @@ import { ShareUserCard } from 'components';
 
 const ShareModal = () => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [isSelected, setIsSelected] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const debounceSerachUser = useDebounce(inputValue, 300);
+
+  const isUserSelected = (userId: string) => {
+    setIsSelected(prev => {
+      const prevSelectedUser = prev.includes(userId);
+
+      return prevSelectedUser
+        ? prev.filter(user => user !== userId)
+        : [...prev, userId];
+    });
+  };
+
+  const handleDivClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
   return (
     <div className={S.ShareModalWrapper}>
       <div className={S.CancelIconWrapper}>
         <I.CancelIcon />
       </div>
-      <div className={S.SearchInputWrapper}>
+      <div className={S.SearchInputWrapper} onClick={handleDivClick}>
         <I.SearchIcon color={inputValue.length > 0 ? 'white' : 'gray'} />
         <input
           className={S.SearchInput}
           placeholder="검색"
           onChange={e => setInputValue(e.target.value)}
+          ref={inputRef}
         />
       </div>
       <div className={S.UserCardBox}>
         {shareUserTestData
-          .filter(userid =>
+          .filter(userInfo =>
             debounceSerachUser.length <= 0
               ? true
               : debounceSerachUser
                   .split(',')
                   .filter(term => term.trim() !== '')
-                  .some(i => userid.userId.includes(i.trim())),
+                  .some(i => userInfo.userId.includes(i.trim())),
           )
           .map((userInfo, i) => {
-            return <ShareUserCard data={userInfo} key={i} />;
+            return (
+              <ShareUserCard
+                data={userInfo}
+                key={i}
+                isChecked={isSelected.includes(userInfo.userId)}
+                onCardClick={() => isUserSelected(userInfo.userId)}
+              />
+            );
           })}
       </div>
       <button className={S.ShareButton}>보내기</button>
