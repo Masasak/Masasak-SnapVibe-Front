e5c010da7f97f46a12a2577f11a59285bf2ecbe9
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-02-03 00:18:20 +0900

    Add sharemodal storybook

---
+++ src/components/ShareModal/index.stories.tsx
@@ -0,0 +1,14 @@
+import ShareModal from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'ShareModal',
+  component: ShareModal,
+} as Meta<typeof ShareModal>;
+
+type Story = StoryObj<typeof ShareModal>;
+
+export const Primary: Story = {
+  args: {},
+};
