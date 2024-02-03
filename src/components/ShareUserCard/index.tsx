bae5c92311aac890899fb39b856fc8ddcb5d6495
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-02-04 01:51:19 +0900

    Update code refactoring

--- src/components/ShareUserCard/index.tsx
+++ src/components/ShareUserCard/index.tsx
@@ -18,20 +18,18 @@ const ShareUserCard: React.FC<ShareUserCardProps> = ({
   data: { profileImgUrl, userId },
   isChecked,
   onCardClick,
-}) => {
-  return (
-    <div className={S.UserCardWrapper}>
-      <div className={S.UserInfoWrapper}>
-        <div className={S.UserImgWrapper}>
-          <Image src={profileImgUrl} alt="유저사진" fill />
-        </div>
-        <div className={S.UserId}>{userId}</div>
+}) => (
+  <div className={S.UserCardWrapper}>
+    <div className={S.UserInfoWrapper}>
+      <div className={S.UserImgWrapper}>
+        <Image src={profileImgUrl} alt="유저사진" fill />
       </div>
-      <div className={S.CheckIconWrapper} onClick={onCardClick}>
-        <I.CheckIcon isChecked={isChecked} />
-      </div>
+      <div className={S.UserId}>{userId}</div>
     </div>
-  );
-};
+    <div className={S.CheckIconWrapper} onClick={onCardClick}>
+      <I.CheckIcon isChecked={isChecked} />
+    </div>
+  </div>
+);
 
 export default ShareUserCard;
