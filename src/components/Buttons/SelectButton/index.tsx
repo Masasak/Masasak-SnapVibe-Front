3be29de7425e994bfab798eee02a665b7c2c4ef3
TaerangLee <s22051@gsm.hs.kr>
2024-02-10 10:29:21 +0900

    Update selectButtonProps isSelect

--- src/components/Buttons/SelectButton/index.tsx
+++ src/components/Buttons/SelectButton/index.tsx
@@ -4,10 +4,24 @@ import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
 import * as S from './index.css';
 
-const SelectButton: React.FC<
-  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
-> = ({ children = '', ...attributes }) => (
-  <button className={S.ButtonStyle} {...attributes}>
+interface SelectButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  isSelect: boolean;
+}
+
+const SelectButton: React.FC<SelectButtonProps> = ({
+  children = '',
+  isSelect,
+  ...attributes
+}) => (
+  <button
+    className={S.ButtonStyle}
+    {...attributes}
+    style={{ border: isSelect ? '1px solid white' : '' }}
+  >
     {children}
   </button>
 );
