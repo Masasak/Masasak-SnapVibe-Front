c4be310c0ca2ffaa025e525ed74a87481fc2fa60
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-03-12 23:39:49 +0900

    Update next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['*'],
+  },
+};
 
 export default withVanillaExtract(nextConfig);
