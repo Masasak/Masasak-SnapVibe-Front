a58453fbf8bfe4b65bf4c75cfe44c10f95926002
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-03-18 19:50:16 +0900

    Update Style

--- src/components/MyPageUserInfo/index.css.ts
+++ src/components/MyPageUserInfo/index.css.ts
@@ -5,7 +5,6 @@ export const MyPageUserBox = style({
   width: '82.5rem',
   height: '286px',
   position: 'relative',
-  backgroundColor: 'blue',
 });
 
 export const CoverImgWrapper = style({
@@ -31,100 +30,74 @@ export const ChangeCoverButton = style({
   alignItems: 'center',
   justifyContent: 'center',
   cursor: 'pointer',
+  zIndex: '1',
 });
 
 export const UserInfoWrapper = style({
   width: '75.625rem',
   height: '9.375rem',
-  backgroundColor: 'red',
   position: 'absolute',
   top: '8.5rem',
   left: '5.625rem',
   display: 'flex',
-  justifyContent: 'space-between',
-});
-
-export const UserProfileWrapper = style({
-  width: '28.0625rem',
-  height: '9.375rem',
-  backgroundColor: 'yellow',
-  display: 'flex',
-  justifyContent: 'space-between',
-  position: 'relative',
 });
 
 export const ProfileImgWrapper = style({
   width: '9.375rem',
   height: '9.375rem',
   overflow: 'hidden',
-  zIndex: '1',
   borderRadius: '100%',
   backgroundColor: 'pink',
   position: 'relative',
+  marginRight: '2.25rem',
+  zIndex: '1',
 });
 
-export const ProfileContentsWrapper = style({
-  width: '16.4375rem',
-  height: '9.375rem',
-  backgroundColor: 'green',
+export const ContentWrap = style({
+  flex: '1',
+  height: '5.375rempx',
+  marginTop: '4rem',
 });
 
 export const UserId = style({
   ...theme.typo.h4,
-  fontWeight: '500',
   color: theme.grayScale.white,
-  margin: '4.625rem 0 0.75rem 0',
+  margin: '0.625rem 0 0.75rem 0.25rem',
 });
 
-export const ButtonWrapeer = style({
+export const ButtonWrapper = style({
   width: '100%',
-  height: '40px',
+  height: '2.5rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const FollowshipButtonWrapper = style({
+  width: '16.4375rem',
+  height: '2.5rem',
   display: 'flex',
   justifyContent: 'space-between',
 });
 
-export const FollowshipButton = style({
-  width: 'fit-content',
-  height: 'fit-content',
-  padding: ' 0.5625rem 1.125rem 0.625rem 1.125rem',
+export const FollowerButton = style({
+  width: '8.3125rem',
+  height: '2.5rem',
+  backgroundColor: '#242424',
   ...theme.typo.Body1,
   fontWeight: '500',
   color: theme.grayScale.white,
   borderRadius: '0.625rem',
-  backgroundColor: '#242424',
 });
 
-// export const ButtonContainer = style({
-//   width: '16.4375rem',
-//   height: '100%',
-//   display: 'flex',
-//   justifyContent: 'space-between',
-// });
-
-// export const FollowerButton = style({
-//   width: '8.3125rem',
-//   height: '2.5rem',
-//   backgroundColor: '#242424',
-//   ...theme.typo.Body1,
-//   fontWeight: '500',
-//   color: theme.grayScale.white,
-//   borderRadius: '0.625rem',
-// });
-
-// export const FollowingButton = style({
-//   width: '7.375rem',
-//   height: '2.5rem',
-//   backgroundColor: '#242424',
-//   ...theme.typo.Body1,
-//   fontWeight: '500',
-//   color: theme.grayScale.white,
-//   borderRadius: '0.625rem',
-// });
-
-export const HandleInfoButtonWrapper = style({
-  display: 'flex',
-  gap: '0.5rem',
-  paddingTop: '56px',
+export const FollowingButton = style({
+  width: '7.375rem',
+  height: '2.5rem',
+  backgroundColor: '#242424',
+  ...theme.typo.Body1,
+  fontWeight: '500',
+  color: theme.grayScale.white,
+  borderRadius: '0.625rem',
 });
 
 export const HandleInfoButton = style({
