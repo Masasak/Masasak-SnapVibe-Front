940a164b13286b869b4336078566394cd77338e3
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-03-18 19:50:39 +0900

    Add MyPageUserInfo storybook

---
+++ src/components/MyPageUserInfo/index.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react';
+import MyPageUserInfo from '.';
+
+export default {
+  title: 'MyPageUserInfo',
+  component: MyPageUserInfo,
+} as Meta<typeof MyPageUserInfo>;
+
+type Story = StoryObj<typeof MyPageUserInfo>;
+
+export const Primary: Story = {};
