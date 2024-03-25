14ad11eae28df1a0f51aea55bffe8ad21f47dc65
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-03-25 15:52:33 +0900

    Update const renaming

--- src/assets/svg/CancelIcon.tsx
+++ src/assets/svg/CancelIcon.tsx
@@ -1,27 +1,24 @@
 interface CancelIconProps {
   size?: string;
 }
+const DEFAULT_SIZE = '1.5rem' as const;
 
-const CacnelIcon: React.FC<CancelIconProps> = ({ size }) => {
-  const defaultSize = '1.5rem';
-
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size ? size : defaultSize}
-      height={size ? size : defaultSize}
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <path
-        d="M6 18L18 6M6 6L18 18"
-        stroke="white"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
+const CacnelIcon: React.FC<CancelIconProps> = ({ size }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size ? size : DEFAULT_SIZE}
+    height={size ? size : DEFAULT_SIZE}
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M6 18L18 6M6 6L18 18"
+      stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 export default CacnelIcon;
