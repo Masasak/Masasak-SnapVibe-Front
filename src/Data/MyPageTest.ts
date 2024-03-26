6791077ae3b9097d943995b3225788ccf82046a9
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-03-26 09:40:00 +0900

    Add MyPageTest data

---
+++ src/Data/MyPageTest.ts
@@ -0,0 +1,15 @@
+import { myPageTypes } from 'types';
+
+export const MyPageTest: myPageTypes[] = [
+  {
+    user: {
+      id: '123',
+      name: '이동욱',
+      nickName: 'dongwook1207',
+      followers: 1000,
+      followings: 199,
+      profileImageUrl: '',
+      profileCoverImage: '',
+    },
+  },
+];
