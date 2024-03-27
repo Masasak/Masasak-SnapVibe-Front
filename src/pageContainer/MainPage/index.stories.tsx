76669d51c89e69e620a2970566560f813eb24a03
TaerangLee <s22051@gsm.hs.kr>
2024-03-27 20:23:33 +0900

    Add mainPage storybook

---
+++ src/pageContainer/MainPage/index.stories.tsx
@@ -0,0 +1,12 @@
+import MainPage from './';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'MainPage',
+  component: MainPage,
+} as Meta<typeof MainPage>;
+
+type Story = StoryObj<typeof MainPage>;
+
+export const Primary: Story = {};
