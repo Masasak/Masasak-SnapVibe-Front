8185e19d43954665076b05d1e9b555b11061fae4
TaerangLee <s22051@gsm.hs.kr>
2024-03-27 20:30:09 +0900

    Fix myPage workspace organize

--- src/pageContainer/MyPage/index.tsx
+++ src/pageContainer/MyPage/index.tsx
@@ -6,24 +6,21 @@ import { shareUserTest, MyPageTest } from 'Data';
 
 import * as S from './index.css';
 
-const MyPage = () => {
+const MyPage = () => (
   //추후 데이터 받는 로직 작성
-  return (
-    <div className={S.PageWrapper}>
-      <Header />
-      <div className={S.ContentWrapper}>
-        <MyPageUserInfo data={MyPageTest} />
-        <div className={S.PostWrapper}>
-          <p className={S.Title}>내 게시물</p>
-          <div className={S.PostCardWrapper}>
-            {shareUserTest.map(userInfo => (
-              <PostCard data={userInfo} key={userInfo.author.id} />
-            ))}
-          </div>
+  <div className={S.PageWrapper}>
+    <Header />
+    <div className={S.ContentWrapper}>
+      <MyPageUserInfo data={MyPageTest} />
+      <div className={S.PostWrapper}>
+        <p className={S.Title}>내 게시물</p>
+        <div className={S.PostCardWrapper}>
+          {shareUserTest.map(userInfo => (
+            <PostCard data={userInfo} key={userInfo.author.id} />
+          ))}
         </div>
       </div>
     </div>
-  );
-};
-
+  </div>
+);
 export default MyPage;
